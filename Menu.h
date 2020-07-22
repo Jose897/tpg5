af66f50d1f84841a3ed660fea9552ae4ff0cecd5
lebarrionuevo <66890327+lebarrionuevo@users.noreply.github.com>
2020-07-21 21:10:18 -0300

    Agrego atributos para las opciones

--- Menu.h
+++ Menu.h
@@ -14,6 +14,10 @@ public:
     void mostrar_menu();
     bool opcion_valida(int entrada);
     void procesar_opcion(int opcion);
+    void procesar_opcion_uno();         //Se le debe pasar un puntero al arbol
+    void procesar_opcion_dos();         //Se le debe pasar un puntero al arbol
+    void procesar_opcion_tres();        //Se le debe pasar un puntero al arbol
+    void procesar_opcion_cuatro();      //Se le debe pasar un puntero al arbol
 };
 
 #endif //TPG5_MENU_H
