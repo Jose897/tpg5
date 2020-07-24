ae0038b05982152f492f66e1eab2e620a11717bf
lebarrionuevo <66890327+lebarrionuevo@users.noreply.github.com>
2020-07-23 23:31:05 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,40 +4,23 @@
 #include<iostream>
 #include<string>
 #include "Archivo.h"
+
 const string NOMBRE_ARCHIVO_AEROPUERTO = "aeropuertos.txt";
 
 using namespace std;
 int main(){
-    //==============EJEMPLO PARA VER COMO FUNCIONA LA CLASE AEROPUERTO=================
-    Archivo archivo;
-    archivo.lectura_aeropuertos(NOMBRE_ARCHIVO_AEROPUERTO);
-   /* Menu menu;
-    menu.mostrar_menu();*/
-    string nom = "Ministro_Pistarini";
-    string ciudad = "Ezeiza";
-    string pais = "Argentina";
-    float superficie = 34.75;
-    int terminales = 4;
-    int nacionales = 12;
-    int internacionales = 46;
-    Aeropuertos aero(nom, ciudad, pais, superficie, terminales, nacionales, internacionales);
-    aero.mostrar();
-    /*==============EJEMPLO PARA VER COMO FUNCIONA EL DICCIONARIO EN EL ABB=================
+    /* Menu menu;
+    menu.mostrar_menu();
+    ==============EJEMPLO PARA VER COMO FUNCIONA EL DICCIONARIO EN EL ABB=================
     COMENTARIOS: Hay que revisar como eliminar elementos, con el remove solo me setea la clave en 0.
-    Tambien ver como setear nuevas claves e interactuar para en una clave poner mas valores o eliminar algunos.
-    string entrada;
-    BST<int>* bst = new BST<int>();
-    int datos[] = { 23, 12, 31, 3, 7, 15, 29, 88, 53};
-    for(int i=0;i<9;i++){
-        cout << "Para la clave '" << datos[i] << "' elegir el valor: ";
-        cin >> entrada;
-        bst->insert(datos[i], entrada);
-    }
+    Tambien ver como setear nuevas claves e interactuar para en una clave poner mas valores o eliminar algunos.*/
+    BST<string>* bst = new BST<string>();
+    Archivo archivo;
+    archivo.lectura_aeropuertos(NOMBRE_ARCHIVO_AEROPUERTO, bst);
     bst->print_in_order();
     //bst -> remove(7); Esta linea no funciona correctamente por lo mencionado en los comentarios
-
-    delete bst;*/
-
+    delete bst;
     return 0;
 
 }
+
