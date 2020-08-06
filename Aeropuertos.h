08897a9a644616534074e48facea720a7ae16a3e
lebarrionuevo <66890327+lebarrionuevo@users.noreply.github.com>
2020-08-06 20:23:42 -0300

    PRE y POST condiciones

--- Aeropuertos.h
+++ Aeropuertos.h
@@ -16,14 +16,33 @@ class Aeropuertos{
         int destinos_nacionales;
         int destinos_internacionales;
     public:
+        // Constructor
+        // PRE: -
+        // POST: Crea el objeto Aeropuerto
         Aeropuertos(string, string, string, float, int, int, int);
+        // PRE: -
+        // POST: Devuelve el nombre
         string obtener_nombre();
+        // PRE: -
+        // POST: Devuelve la ciudad
         string obtener_ciudad();
+        // PRE: -
+        // POST: Devuelve el pais
         string obtener_pais();
+        // PRE: -
+        // POST: Devuelve el valor de la superifcie
         float obtener_superficie();
+        // PRE: -
+        // POST: Devuelve la cantidad terminales
         int obtener_terminales();
+        // PRE: -
+        // POST: Devuelve la cantidad de destinos nacionales
         int obtener_destinos_nacionales();
+        // PRE: Objeto Aeropuerto creado
+        // POST: Devuelve la cantidad de destinos internacionales
         int obtener_destinos_internacionales();
+        // PRE: Objeto Aeropuerto creado
+        // POST: Muestra los datos del objeto de forma ordenada
         void mostrar();
 };
 
